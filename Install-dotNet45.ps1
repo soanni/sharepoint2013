43ed3fdf572a563457d27aab03bf546d3b2a2b03
Andrey Solodov <andrey.solodov@aurea.com>
2016-10-11 16:45:30 +0000

    prerequisites added

---
+++ Install-dotNet45.ps1
@@ -0,0 +1,11 @@
+﻿$TempFolder = "C:\sources"
+if(!(test-path $TempFolder))
+{
+    New-Item -path $TempFolder -type directory
+}
+  
+Write-Host ""
+$dest = "$TempFolder" + "\" + "dotnetfx45_full_x86_x64.exe"
+$args = " /q /norestart"
+Write-Host "Installing .NET framework v4.5" -ForegroundColor Yellow;
+Start-Process -FilePath $dest -ArgumentList $args -Wait
